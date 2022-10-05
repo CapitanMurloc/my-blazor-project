f066407483ae975f3c75557b73f2aa2d572e0cbb
javier.estrella <javier.estrella@henryschein.es>
2022-10-05 16:47:17 +0200

    Add Camera

---
+++ my-blazor-project/wwwroot/webcam.js
@@ -0,0 +1,26 @@
+﻿export function startVideo(src) {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+            let video = document.getElementById(src);
+            if ("srcObject" in video) {
+                video.srcObject = stream;
+            } else {
+                video.src = window.URL.createObjectURL(stream);
+            }
+            video.onloadedmetadata = function (e) {
+                video.play();
+            };
+            //mirror image
+            video.style.transform = "scaleX(-1)";
+        });
+    }
+}
+
+export function getFrame(src, dest, dotNetHelper) {
+    let video = document.getElementById(src);
+    let canvas = document.getElementById(dest);
+    canvas.getContext('2d').drawImage(video, 0, 0, 320, 240);
+
+    let dataUrl = canvas.toDataURL("image/jpeg");
+    dotNetHelper.invokeMethod('ProcessImage', dataUrl);
+}
